a76eca5e4cb497bd6497069bf98cc5a92ae24ebe
Ngoc Nguyen <30312571+nooeen@users.noreply.github.com>
2022-06-14 19:00:51 +0700

    finish custom hook app

---
+++ realistic-custom-hook-app/src/components/NewTask/NewTask.js
@@ -0,0 +1,37 @@
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHTTP from "../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHTTP();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: "https://react-max-academind-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
