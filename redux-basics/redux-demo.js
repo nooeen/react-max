98e2c8f77b5db4b7bad2c34ec2f46f1e0f3c4611
Ngoc Nguyen <30312571+nooeen@users.noreply.github.com>
2022-06-25 11:18:34 +0700

    finish redux basics app & init redux-react basics app

--- redux-basics/redux-demo.js
+++ redux-basics/redux-demo.js
@@ -1,7 +1,13 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return { counter: state.counter + 1 };
+  if (action.type === "increment") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  }
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -14,3 +20,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
